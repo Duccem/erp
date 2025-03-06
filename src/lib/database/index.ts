3162972599e09697183ac68371b0a9165a403b4f
Jose Veliz <ducen29@gmail.com>
2025-03-06 17:16:10 -0400

    database

---
+++ src/lib/database/index.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from "@prisma/client";
+import "server-only";
+
+const createPrismaClient = () =>
+  new PrismaClient({
+    log: ["error"],
+  });
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: ReturnType<typeof createPrismaClient> | undefined;
+};
+
+export const database = globalForPrisma.prisma ?? createPrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = database;
+
