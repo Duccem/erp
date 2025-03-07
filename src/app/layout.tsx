85e6a2d5f80adc6a168764af149ec9144c2a450c
Jose Veliz <ducen29@gmail.com>
2025-03-07 13:02:09 -0400

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,15 @@
-import "@/assets/globals.css";
-import type { Metadata } from "next";
-import { Nunito } from "next/font/google";
-import { Providers } from "./providers";
+import '@/assets/globals.css';
+import iconDark from '@/assets/images/lumen-dark.png';
+import icon from '@/assets/images/lumen-light.png';
+import type { Metadata } from 'next';
+import { Nunito } from 'next/font/google';
+import { Providers } from './providers';
 
-const nunito = Nunito({ subsets: ["latin"] });
+const nunito = Nunito({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Ducen ERP",
-  description: "Powered AI Administrative Assistant",
+  title: 'Ducen ERP',
+  description: 'Powered AI Administrative Assistant',
 };
 
 export default function RootLayout({
@@ -17,13 +19,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${nunito.className} antialiased`}
-        suppressHydrationWarning
-      >
+      <link rel="icon" href={icon.src} sizes="any" className="hidden dark:block" />
+      <link rel="icon" href={iconDark.src} sizes="any" className="dark:hidden" />
+      <body className={`${nunito.className} antialiased`} suppressHydrationWarning>
         <Providers>{children}</Providers>
       </body>
     </html>
   );
 }
-
